d010521a928b55189b4da0adbcce9be55733cf2e
Hakan <hakan_merts@hotmail.com>
2025-05-26 22:52:17 +0300

    15 done

--- src/test/java/JDBC/Queries.java
+++ src/test/java/JDBC/Queries.java
@@ -3,6 +3,7 @@ package JDBC;
 import org.testng.annotations.Test;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Queries extends DatabaseHelper {
 
@@ -169,6 +170,30 @@ public class Queries extends DatabaseHelper {
     // göster.
     // 15. Find the Employee with the Highest Salary Average in the Research Department
     // - Araştırma Departmanındaki Ortalama En Yüksek Maaşlı Çalışanı Bul
+    @Test
+    public void TC15() throws SQLException {
+
+        String sorgu =
+                "select employees.emp_no, employees.first_name, employees.last_name, avg(salaries.salary) as ortalama_maas \n" +
+                        "from employees \n" +
+                        "JOIN dept_emp ON employees.emp_no = dept_emp.emp_no \n" +
+                        "JOIN salaries ON employees.emp_no = salaries.emp_no \n" +
+                        "where dept_emp.dept_no = 'd008' \n" +
+                        "group by employees.emp_no, employees.first_name, employees.last_name \n" +
+                        "order by ortalama_maas desc \n" +
+                        "limit 1;";
+
+        ArrayList<ArrayList<String>> sonuc =DatabaseHelper.getListData(sorgu);
+
+        // Başlıklar
+        System.out.printf("%-15s %-15s %-20s%n", "Ortalama Maaş", "Ad", "Soyad");
+        System.out.println("----------------------------------------------------------");
+
+        // Satırları yazdır
+        for (ArrayList<String> satir : sonuc) {
+            System.out.printf("%-15s %-15s %-20s%n", satir.get(0), satir.get(1), satir.get(2));
+        }
+    }
     // 16. For each department, identify the employee with the highest single salary ever recorded. List the
     // department name, employee's first name, last name, and the peak salary amount. Order the results
     // by the peak salary in descending order.
@@ -285,6 +310,5 @@ public class Queries extends DatabaseHelper {
     // -- Nisan 1992'de işe alınan çalışanları listeleme
     // 41. Find all departments that employee '10102' has worked in.
     // -- '10102' numaralı çalışanın çalıştığı tüm bölümleri bulma.
-
 
 }
